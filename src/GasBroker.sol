de92b23a4e2e882bc4978dd48a9d83ef72297dc0
Oleksandr Koval <alexsanyakoval@gmail.com>
2023-10-10 16:53:11 -0500

    add public function getEthAmount

--- src/GasBroker.sol
+++ src/GasBroker.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 
 interface IPriceOracle {
   function WETH() external view returns (address);
-  function getPriceInEth(address token, uint amount, uint32 twapPeriod) external view returns (uint256);
+  function getPriceInEth(address token, uint amount) external view returns (uint256);
 }
 
 interface IERC2612 {
@@ -14,20 +15,22 @@ interface IERC2612 {
 
 struct Reward {
     uint256 value;
-    uint256 permitHash; //keccak256 for permit signature
+    bytes32 permitHash; //keccak256 for permit signature
 }
 
 contract GasBroker {
-  string private constant REWARD_TYPE = "Reward(uint256 value,uint256 permitHash)";
+  using Address for address payable;
+
+  string private constant REWARD_TYPE = "Reward(uint256 value,bytes32 permitHash)";
   string private constant EIP712_DOMAIN = "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)";
-  uint256 constant TWAP_PERIOD = 180;
+  uint32 constant TWAP_PERIOD = 180;
   bytes32 public immutable REWARD_DOMAIN_SEPARATOR;
   IPriceOracle immutable priceOracle;
 
   constructor(uint256 chainId, address _priceOracle) {
     REWARD_DOMAIN_SEPARATOR = keccak256(
       abi.encode(
-        keccak256(EIP712_DOMAIN),
+        keccak256(abi.encode(EIP712_DOMAIN)),
         keccak256("Gas broker"),
         keccak256("1"),
         chainId,
@@ -51,7 +54,7 @@ contract GasBroker {
     bytes32 rewardS) external payable {
       require(value > reward, "Reward could not exceed value");
 
-      uint256 permitHash = keccak256(abi.encode(permitV,permitR,permitS));
+      bytes32 permitHash = keccak256(abi.encode(permitV,permitR,permitS));
       require(verifyReward(signer, Reward(reward, permitHash), rewardV, rewardR, rewardS), "Reward signature is invalid");
       token.permit(
         signer,
@@ -62,20 +65,20 @@ contract GasBroker {
         permitR,
         permitS
       );
-      uint256 ethAmount = priceOracle.getPriceInEth(address(token), value - reward, TWAP_PERIOD);
+      uint256 ethAmount = _getEthAmount(address(token), value - reward);
       require(msg.value >= ethAmount, "Not enough ETH provided");
       payable(signer).sendValue(ethAmount);
-      SafeERC20.safeTransfer(address(token), msg.sender, value);
+      SafeERC20.safeTransfer(IERC20(address(token)), msg.sender, value);
     }
 
-    function hashReward(Reward memory reward) private pure returns (bytes32) {
+    function hashReward(Reward memory reward) private view returns (bytes32) {
       return keccak256(
         abi.encodePacked(
           "\\x19\\x01",
           REWARD_DOMAIN_SEPARATOR,
           keccak256(
             abi.encode(
-              keccak256(REWARD_TYPE),
+              keccak256(abi.encode(REWARD_TYPE)),
               reward.value,
               reward.permitHash
             )
@@ -90,7 +93,16 @@ contract GasBroker {
       uint8 sigV,
       bytes32 sigR,
       bytes32 sigS
-    ) private pure returns (bool) {
+    ) private view returns (bool) {
       return signer == ecrecover(hashReward(reward), sigV, sigR, sigS);
     }
+
+    function _getEthAmount(address token, uint256 amount) internal view returns (uint256 ethAmount) {
+      ethAmount = priceOracle.getPriceInEth(address(token), amount);
+    }
+    
+    function getEthAmount(address token, uint256 amount) external view returns (uint256 ethAmount) {
+      ethAmount = _getEthAmount(token, amount);
+    }
+
 }
