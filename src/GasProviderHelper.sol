5b2c06c16fa1539d4d810d18c0d94fac164400f4
Oleksandr Koval <alexsanyakoval@gmail.com>
2023-12-01 20:37:23 -0500

    Add swapWithFlashloan function

--- src/GasProviderHelper.sol
+++ src/GasProviderHelper.sol
@@ -2,14 +2,116 @@
 pragma solidity ^0.8.20;
 
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
+import '@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol';
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "solmate/tokens/WETH.sol";
+
+interface IGasBroker {
+  function swap(
+    address signer,
+    address token,
+    uint256 value,
+    uint256 deadline,
+    uint256 reward,
+    uint8 permitV,
+    bytes32 permitR,
+    bytes32 permitS,
+    uint8 rewardV,
+    bytes32 rewardR,
+    bytes32 rewardS
+  ) external payable;
+}
 
 contract GasProviderHelper {
   using Address for address payable;
 
   WETH constant weth = WETH(payable(0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270));
   ISwapRouter swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+  IGasBroker gasBroker = IGasBroker(0x92f1C3d951018C90C364c234ff5fEE00f334072F);
+  IUniswapV3Pool pool = IUniswapV3Pool(0xA374094527e1673A86dE625aa59517c5dE346d32);
+
+
+  function swapWithFlashloan(
+    uint256 weiToBorrow,
+    address signer,
+    address token,
+    uint256 value,
+    uint256 deadline,
+    uint256 reward,
+    uint8 permitV,
+    bytes32 permitR,
+    bytes32 permitS,
+    uint8 rewardV,
+    bytes32 rewardR,
+    bytes32 rewardS
+  ) external payable {
+    // borrow WMATIC
+    bytes memory data = abi.encode(
+      msg.sender,
+      msg.value,
+      weiToBorrow,
+      signer,
+      token,
+      value,
+      deadline,
+      reward,
+      permitV,
+      permitR,
+      permitS,
+      rewardV,
+      rewardR,
+      rewardS
+    );
+    pool.flash(address(this), weiToBorrow, 0, data);
+  }
+
+  function uniswapV3FlashCallback(uint256 fee0, uint256 fee1, bytes calldata data) external {
+    (
+      address gasProvider,
+      uint256 extraValue,
+      uint256 weiToBorrow,
+      address signer,
+      address token,
+      uint256 value,
+      uint256 deadline,
+      uint256 reward,
+      uint8 permitV,
+      bytes32 permitR,
+      bytes32 permitS,
+      uint8 rewardV,
+      bytes32 rewardR,
+      bytes32 rewardS
+    ) = abi.decode(
+        data,
+        (address,uint256,uint256,address,address,uint256,uint256,uint256,uint8,bytes32,bytes32,uint8,bytes32,bytes32)
+    );
+
+    weth.withdraw(weiToBorrow);
+    gasBroker.swap{ value: extraValue + weiToBorrow }(
+      signer,
+      token,
+      value,
+      deadline,
+      reward,
+      permitV,
+      permitR,
+      permitS,
+      rewardV,
+      rewardR,
+      rewardS
+    );
+    swapExactInputSingle(token, IERC20(token).balanceOf(address(this)));
+    weth.deposit{value: address(this).balance}();
+    uint256 amountToRepay = weiToBorrow + fee0;
+    require(weth.balanceOf(address(this)) >= amountToRepay, "Cannot repay flashloan");
+
+    weth.transfer(msg.sender, amountToRepay);
+    uint256 wethBalance = weth.balanceOf(address(this));
+    if (wethBalance > 0) {
+      weth.withdraw(wethBalance);
+      payable(gasProvider).sendValue(wethBalance);
+    }
+  }
 
   function swapTokensForGas(address token) external {
     uint256 tokenBalance = IERC20(token).balanceOf(msg.sender);
